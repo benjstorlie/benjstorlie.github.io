cb31ff0b423c518b1ec6d27b226a22ad897fe594
bjs <127002178+benstorlie@users.noreply.github.com>
2023-08-17 21:34:53 -0700

    getting readme's to render

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { ReactComponent as GitHubLogo } from '../github-mark-white.svg';
 
 export default function Header() {
   return (
-    <div>Header Content</div>
+    <div style={{background: '#000', color: '#fff'}}>
+      Header Content 
+      <div>
+        <a href='https://github.com/benjstorlie/' target='_blank' rel="noreferrer"><GitHubLogo title='Link to My GitHub Page'/></a>
+      </div>
+    </div>
   );
 }
 
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {useParams} from 'react-router-dom';
 import { request } from "@octokit/request";
-import ReactMarkdown from 'react-markdown';
+import parse from 'html-react-parser';
 
 export default function Project() {
   const { repo } = useParams();
@@ -14,12 +14,17 @@ export default function Project() {
         const response = await request(`GET /repos/benjstorlie/${repo}/readme`, {
           owner: 'benjstorlie',
           repo,
+          mode: 'gfm',
+          context: `benjstorlie/${repo}`,
           headers: {
+            accept: 'application/vnd.github.html+json',
             'X-GitHub-Api-Version': '2022-11-28'
+          },
+          mediaType: {
+            format: 'html'
           }
         })
-        const content = Buffer.from(response.data.content, 'base64').toString('utf-8');
-        setReadmeContent(content);
+        setReadmeContent(response.data);
       } catch (error) {
         console.error('Error fetching readme:', error.message);
       }
@@ -27,5 +32,5 @@ export default function Project() {
     fetchReadme();
   }, [repo]) 
 
-  return (<ReactMarkdown>{readmeContent}</ReactMarkdown>)
+  return (<div>{parse(readmeContent)}</div>)
 }
