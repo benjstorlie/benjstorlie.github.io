632f70a0aafb81c22f91d693c8e57c16b962c5f2
bjs <127002178+benstorlie@users.noreply.github.com>
2023-08-28 17:28:33 -0700

    fixed portfolio routing problem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 
 import { Route, Routes } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+
 import Menu from './pages/Menu'
+import Portfolio from './pages/Portfolio';
 import Project from './pages/Project'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import AboutMe from './pages/AboutMe'
-import Container from 'react-bootstrap/Container';
 import Contact from './pages/Contact'
 import ResumePage from './pages/ResumePage'
 
@@ -16,11 +18,13 @@ function App() {
       <Header />
         <Container fluid id="main">
           <Routes>
-            <Route path='/portfolio' element={ <Menu />} />
+            <Route path='/portfolio' element={ <Portfolio />} >
+              <Route index element={<Menu />} />
+              <Route path=':repo' element={ <Project />} />
+            </Route>
             <Route path='/' element={ <AboutMe />} />
             <Route path='/contact' element={ <Contact />} />
             <Route path='/resume' element={ <ResumePage />} />
-            <Route path='/:repo' element={ <Project />} />
           </Routes>
         </Container>
       <Footer />
