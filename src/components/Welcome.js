f928b54851474862ee22d41e582866a670f9a169
benstorlie <127002178+benjstorlie@users.noreply.github.com>
2024-03-23 19:10:28 -0700

    add link to old portfolio

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -15,7 +15,10 @@ export default function Welcome() {
       </Col>
       <Col xs={12} sm>
         <p>
-        Interested in math, coding, disability advocacy, and speech &amp; language. I have experience in public education as a paraprofessional for young adults. I have a B.A. in Math with some grad school for Math and Math Education. I'd like to be part of Augmentative &amp; Alternative Communication technology, or accessibility/educational technology in general.
+        Interested in math, coding, disability advocacy, and speech &amp; language. I have experience in Special Education as a paraprofessional for young adults. I have a B.A. in Math with some grad school for Math and Math Education. I'd like to be part of Augmentative &amp; Alternative Communication technology, or accessibility/educational technology in general.
+        </p>
+        <p>
+          <a href='https://bentleystorlie.blogspot.com/' target="_blank" rel="noopener noreferrer">Link to my old portfolio.</a> This has descriptions of projects I worked on as a paraprofessional, including visual supports using symbols from SymbolStix or BoardMaker. I am in the process of migrating these over.
         </p>
       </Col>
     </>
